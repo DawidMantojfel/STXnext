2ed512412ea1f773d305278e51bc3c5c607e1f23
dawid <dawidmantojfel@gmail.com>
2021-04-14 18:17:42 +0200

    removed unnecessary lines

--- STXnext/settings.py
+++ STXnext/settings.py
@@ -20,18 +20,6 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
-#     secrets = json.load(secrets_file)
-#
-#
-# def get_secret(setting, secrets=secrets):
-#     """Get secret setting or fail with ImproperlyConfigured"""
-#     try:
-#         return secrets[setting]
-#     except KeyError:
-#         raise ImproperlyConfigured("Set the {} setting".format(setting))
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
